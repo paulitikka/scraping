bee74dd2b5a03bf41f123ae32f85f77f1bcc1c01
Pauli Tikka <30927365+paulitikka@users.noreply.github.com>
2020-11-10 11:17:02 +0100

    Streamlining

--- Connecting project files_v_trans_corona_v37b_tikka1120.py
+++ Connecting project files_v_trans_corona_v37b_tikka1120.py
@@ -1,5 +1,6 @@
 # Scrapping many webpages with selenium, BeautifulSoup and parraller driving. Works best in cluster computers,
-# but perfectly ok for PCs and smaller data. Pauli Tikka et al. 5.3.2020 
+# but perfectly ok for PCs and smaller data. Pauli Tikka et al. 5.3.2020/5.11.20 
+#You may need to do the run of this code in Jupyter to work out, make sure that you have fast computer/s and good internet access!
 
 #Some preliminary infos (maybe too many..):
 
@@ -37,6 +38,7 @@
 #https://stackoverflow.com/questions/18557275/how-to-locate-and-insert-a-value-in-a-text-box-input-using-python-selenium
 #https://stackoverflow.com/questions/1629053/typing-enter-return-key-using-python-and-selenium
 #https://stackoverflow.com/questions/56205245/running-execute-script-with-variable-inside-in-python-via-selenium/56213477
+#https://stackoverflow.com/questions/22003302/beautiful-soup-just-get-the-value-inside-the-tag
 
 #Making dates or values:
 #https://stackoverflow.com/questions/993358/creating-a-range-of-dates-in-python
@@ -57,55 +59,41 @@ import pandas as pd #for importing files
 import numpy as np  #for calculations, array manipulations, and fun :)
 import matplotlib.pyplot as plt #for scientifical plots
 import os
-
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
 import datetime
-import time
 from selenium import webdriver  # for webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait  # for implicit and explict waits
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
-
-import time
-import requests
-from bs4 import BeautifulSoup
 from concurrent.futures import ProcessPoolExecutor, as_completed
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+#https://stackoverflow.com/questions/60296873/sessionnotcreatedexception-message-session-not-created-this-version-of-chrome
 #%% All urls, once you have it do not do it many times, it lasts around 15 min..
+
 all_urls = []
 options = Options()
 options.add_argument("--headless")
 prefs = {'profile.managed_default_content_settings.images':2, 'disk-cache-size': 4096}
 options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(executable_path='C:/Users/Pauli/Downloads/chromedriver.exe',options=options)
-
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(executable_path='C:/Users/Pauli/Downloads/chromedriver.exe',options=options)
 driver.get('http://103.247.238.92/dghseams/attend/professional_category_wise.php')
 divs = driver.find_elements_by_class_name('link')
-
 divisions_urls = [{'div_name':div.get_attribute('text'), 'div_url':div.get_attribute('href')} for div in divs]
-
+#%
 for div_url in divisions_urls:
-    driver.get(div_url['div_url'])
-    
-    dists = driver.find_elements_by_class_name('link')
-    
-    districts_urls = [{'dist_name':dist.get_attribute('text'), 'dist_url':dist.get_attribute('href')} for dist in dists]
-    
-    
+    driver.get(div_url['div_url'])   
+    dists = driver.find_elements_by_class_name('link')    
+    districts_urls = [{'dist_name':dist.get_attribute('text'), 'dist_url':dist.get_attribute('href')} for dist in dists]  
     for dist_url in districts_urls:
         driver.get(dist_url['dist_url'])
-
-        upzs = driver.find_elements_by_class_name('link')
-        
+        upzs = driver.find_elements_by_class_name('link')        
         upazilas_urls = [{'div_name':div_url['div_name'],
                           'dist_name':dist_url['dist_name'],
                           'upz_name':upz.get_attribute('text'), 
                           'upz_url':upz.get_attribute('href')} for upz in upzs]
-
         all_urls.append(upazilas_urls)
 #%%list of urls
 flattened_list = [y for x in all_urls for y in x]    
@@ -113,7 +101,7 @@ df = pd.DataFrame(flattened_list, columns = ['div_name', 'dist_name','upz_name',
 #%%test df
 df
 #%%The dates, this should be fast:
-start = datetime.datetime.strptime("2019-11-26", "%Y-%m-%d")
+start = datetime.datetime.strptime("2019-01-26", "%Y-%m-%d")
 end = datetime.datetime.strptime("2020-02-29", "%Y-%m-%d")
 date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 dates=[]
@@ -123,15 +111,12 @@ for date in date_generated:
 dated_urls = []
 for i in range(len(df)):
     for date in dates:
-
         breaked_url = df['upz_url'][i].split("cdate")[0]
         date_url = 'cdate={}&cdate2={}'.format(date,date) 
-        new_url = breaked_url + str(date_url)
-        
+        new_url = breaked_url + str(date_url)        
         new_row = {"division":df['div_name'][i],"district":df['dist_name'][i],
                    "upazila":df['upz_name'][i],"date":date,"url":new_url}
         dated_urls.append(new_row)
-
 #         print(dated_urls)
 #%%dated urls in panda and csv:
 dated_df = pd.DataFrame(dated_urls)
@@ -139,16 +124,12 @@ dated_df.to_csv("dated_urls")
 #%%in the future, you just do this:
 dated_df = pd.read_csv("dated_urls")
 #dated_df  
-
 #%%The parallisation (Thank you Ali for the code):
-def parse_req(url,dated_row):
-    
-    
+def parse_req(url,dated_row):     
     old_names = ['Organization', 'Orgs. with functioning attendance system',
        'Orgs. sending data', 'No. of Posts', 'No. of Posts.1',
        'No. of Posts.2', 'Vacancy(%)', 'No. of staffs', 'No. of staffs.1',
        'No. of staffs.2', 'Attendance (%)']
-
     new_names = ['Organization', 'Orgs. with functioning attendance system',
        'Orgs. sending data', 'Sanctioned', 'Filled', 'Vacant', 'Vacancy(%)',
        'Rgd. in attendance system', 'Present*', 'Absent', 'Attendance (%)']
@@ -170,18 +151,16 @@ def parse_req(url,dated_row):
 
     row = pd.concat([a, b], axis=1)  
 #         print(row)
-    
-    
+       
     return row
 #%%Driving the whole thing for doctors:
 from concurrent.futures import as_completed, ProcessPoolExecutor
 import time
 import numpy as np
-import winprocess
+import winprocess # check the that this is in the right folder
 from bs4 import BeautifulSoup
 import alpine
-#%%
-
+#%
 doctors_df = pd.DataFrame()
 start = time.time()
 
@@ -193,18 +172,15 @@ with ProcessPoolExecutor(max_workers=20) as executor:
         futures.add(future)
 
     for future in as_completed(futures):
-        
         doctors_df = pd.concat([doctors_df, future.result()], axis=0)
         
 end = time.time()
 print("Time Taken: {:.6f}s".format(end-start))   
-#%%saving the data for all doctors
+#%saving the data for all doctors
 doctors_df.reset_index(drop=True, inplace=True)
 doctors_df.drop(columns=['Unnamed: 0'], inplace=True)
 doctors_df
-#%%
 doctors_df.to_csv("all_doctors.csv") 
-
 #%%For the other groups:
 options = Options()
 options.add_argument("--headless")
@@ -212,7 +188,7 @@ prefs = {'profile.managed_default_content_settings.images':2, 'disk-cache-size':
 options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(executable_path='C:/Users/Pauli/Downloads/chromedriver.exe',options=options)
 
-#%%The parse command has similarities:
+#%The parse command has similarities:
 def parse_req_all(url,dated_row):
     
     old_names = ['Organization', 'Orgs. with functioning attendance system',
@@ -223,8 +199,7 @@ def parse_req_all(url,dated_row):
     new_names = ['Organization', 'Orgs. with functioning attendance system',
        'Orgs. sending data', 'Sanctioned', 'Filled', 'Vacant', 'Vacancy(%)',
        'Rgd. in attendance system', 'Present*', 'Absent', 'Attendance (%)']
-    
-    
+       
     driver.get(url);
     
     rows = pd.DataFrame()
@@ -239,17 +214,14 @@ def parse_req_all(url,dated_row):
         search_boxn=driver.find_elements_by_xpath("//input[@name='submit' and @value='show report']")[0]
         search_boxn.click()
     
-    
         soup = BeautifulSoup(driver.page_source, 'lxml')
         soup1 = pd.read_html(str(soup),header=0)
         soup2 = pd.DataFrame(soup1[0])
-
         soup2.rename(columns=dict(zip(old_names, new_names)), inplace=True)
         soup2.drop([0, 2], inplace=True)
 
         a = pd.DataFrame(dated_row).transpose()
         a.reset_index(drop=True, inplace=True)
-
         b = soup2
         b.reset_index(drop=True, inplace=True)
 
@@ -260,14 +232,12 @@ def parse_req_all(url,dated_row):
 
     
     return rows
-#%%
+#%
 from concurrent.futures import as_completed, ProcessPoolExecutor
 import time
 import numpy as np
 import winprocess
 from bs4 import BeautifulSoup
-
-
 all_df = pd.DataFrame()
 start = time.time()
 
@@ -278,15 +248,58 @@ with ProcessPoolExecutor(max_workers=20) as executor:
         future = winprocess.submit(executor, parse_req_all, url,dated_row)
         futures.add(future)
 
-    for future in as_completed(futures):
-        
+    for future in as_completed(futures):       
         all_df = pd.concat([all_df, future.result()], axis=0)
         
 end = time.time()
 print("Time Taken: {:.6f}s".format(end-start))
-#%%
+#%Remember to save your data! 
 all_df.reset_index(drop=True, inplace=True)
 all_df.drop(columns=['Unnamed: 0'], inplace=True)
 all_df
-#%%saving the data for all docros nurses and other group
-all_df.to_csv("all_dfs.csv")    
+#%saving the data for all docros nurses and other group
+all_df.to_csv("all_dfs.csv")  
+
+#%%HERE IS ANOTHER FILE 'winprocess.py', needed above:
+import inspect
+import types
+from bs4 import BeautifulSoup
+from selenium import webdriver  # for webdriver
+from selenium.webdriver.chrome.options import Options  # for suppressing the browser
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(executable_path='C:/Users/Pauli/Downloads/chromedriver.exe',options=options)
+
+
+def execute_source(callback_imports, callback_name, callback_source, args):
+    for callback_import in callback_imports:
+        exec(callback_import, globals())
+    exec('import time' + "\n" + callback_source)
+    callback = locals()[callback_name]
+    return callback(*args)
+
+
+def submit(executor, callback, *args):
+    callback_source = inspect.getsource(callback)
+    callback_imports = list(imports(callback.__globals__))
+    callback_name = callback.__name__
+    future = executor.submit(
+        execute_source,
+        callback_imports, callback_name, callback_source, args
+    )
+    return future
+
+
+def imports(callback_globals):
+    for name, val in list(callback_globals.items()):
+        if isinstance(val, types.ModuleType) and val.__name__ != 'builtins' and val.__name__ != __name__:
+            import_line = 'import ' + val.__name__
+            if val.__name__ != name:
+                import_line += ' as ' + name
+            yield import_line
+
